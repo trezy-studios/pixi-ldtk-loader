f6109aac7a3697349e5dd3bd350ca365370fd7f3
Trezy <t@trezy.com>
2023-08-21 16:11:57 -0500

    test(ldtkloader): add test stub

---
+++ test/LDTKLoader.test.js
@@ -0,0 +1,14 @@
+/* eslint-env mocha */
+
+// Module imports
+import { expect } from 'chai'
+
+
+
+
+
+describe('LDTKLoader', function() {
+	it('shouldn\'t fail', function() {
+		return expect(true).to.be.true
+	})
+})
